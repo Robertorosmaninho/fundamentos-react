4b27fe857887eb93eb2c35af3e5381e4a97d9502
Roberto Rosmaninho <robertogrosmaninho@outlook.com>
2020-12-07 09:16:08 -0300

    Second Componet created

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro' // Por ser um export default não necessariamente o nome do componente deve ser o mesmo que o componente exportado 
+import ComParametro from './components/basicos/ComParamentro'
 
 
 ReactDOM.render(
     <div>
         <Primeiro></Primeiro>
-    
+        <ComParametro 
+            titulo="Situação Aluno:" 
+            aluno="Roberto Neto"
+            nota={9.3}/>
     </div>, 
     document.getElementById('root')
 )
