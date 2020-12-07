182df382d4ccb9f0b02d5b763aff7023644df2e9
Roberto Rosmaninho <robertogrosmaninho@outlook.com>
2020-12-07 11:56:02 -0300

    Creating a Card

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import Primeiro from './components/basicos/Primeiro' // Por ser um export defaul
 import ComParametro from './components/basicos/ComParamentro'
 import Fragmento from './components/basicos/Fragmento'
 import Random from './components/basicos/Aleatorio'
+import Card from './components/layout/Card'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default _ =>
     <div id="app">
         <h1>Fundamentos React</h1>
+        <Card titulo="Exemplo de Card"> 
+        
+        </Card>
         <Random min={1} max={10000}/>
         <Fragmento />
         <ComParametro
---
+++ src/components/layout/Card.jsx
@@ -0,0 +1,12 @@
+import './Card.css';
+import React from "react";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props => {
+    return (
+        <div className="Card">
+            <div className="Title"> {props.titulo}</div>
+            <div className="Content">Conteúdo: </div>
+        </div>
+    )
+}
